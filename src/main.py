47c6a447e6a26460f512b7e415038134fa839a18
Gustavo Kanenemberg <gkannemberg@gmail.com>
2024-03-09 17:37:10 -0300

    Feat: webscraping first draft

--- src/main.py
+++ src/main.py
@@ -5,42 +5,67 @@ from datetime import datetime
 
 CITY_NAME = 'Cascavel'
 
-def scrape_data():
-    scraped_data = {}
+class ImoveisSpider(scrapy.Spider):
+    name = 'imoveis'
+    start_urls = ['https://www.imobiliariacidade.com.br']
+    base_url = 'https://www.imobiliariacidade.com.br/venda/?&pagina={}'  # URL base com o marcador de página
 
-    scraped_data["raw_address"] = "Rua São Damião"
-    scraped_data["created_at"] = datetime.now()
+    def start_requests(self):
+        # Gera solicitações para cada página
+        for page_number in range(1, 64):  # o site possui 64 paginas por conta disso o for passa por todas
+            url = self.base_url.format(page_number)
+            yield scrapy.Request(url, callback=self.parse)
 
-    return scraped_data
+    def parse(self, response):
+        for imovel in response.css('div.anuncio'):
+            titulo = imovel.css('h2::text').get()
+            preco = imovel.css('span.preco::text').get()
+            localizacao = imovel.css('p.localizacao::text').get()
+            
+            yield {
+                'titulo': titulo,
+                'preco': preco,
+                'localizacao': localizacao,
+            }
+
+# def scrape_data():
+#     scraped_data = {}
+
+#     scraped_data["raw_address"] = "Rua São Damião"
+#     scraped_data["created_at"] = datetime.now()
+
+#     return scraped_data
 
 
-def enrich_address(scraped_data):
+# def enrich_address(scraped_data):
 
-    gmaps_request_string = f"{CITY_NAME}, "
+#     gmaps_request_string = f"{CITY_NAME}, "
 
-    enriched_address = googlemaps.search(gmaps_request_string)
+#     enriched_address = googlemaps.search(gmaps_request_string)
 
-    return enriched_address
+#     return enriched_address
 
 
-def save_address_mongo(enriched_data):
+# def save_address_mongo(enriched_data):
 
-    pymongo.save(enriched_data)
+#     pymongo.save(enriched_data)
     
 
 
-def process():
+# def process():
 
-    scraped_data = scrape_data()
+#     scraped_data = scrape_data()
 
-    enriched_data = enrich_address(scraped_data)
+#     enriched_data = enrich_address(scraped_data)
 
-    all_data = scraped_data.update(enriched_data)
+#     all_data = scraped_data.update(enriched_data)
 
-    save_address_mongo(all_data)
+#     save_address_mongo(all_data)
 
 
 
-# process()
+#  process()
+
+
 
     
