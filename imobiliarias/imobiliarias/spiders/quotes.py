f67f82d2779a0e0930ac0071e438335b3dde1374
Gustavo Kanenemberg <gkannemberg@gmail.com>
2024-03-11 06:26:34 -0300

    Feat: responses, next page added.

--- imobiliarias/imobiliarias/spiders/quotes.py
+++ imobiliarias/imobiliarias/spiders/quotes.py
@@ -3,8 +3,27 @@ import scrapy
 
 class QuoteSpider(scrapy.Spider):
     name = "quotes"
-    start_urls = ["https://quotes.toscrape.com/"]
+    start_urls = ["https://www.imobiliariaativa.com.br/pesquisa-de-imoveis/"]
+  
 
     def parse(self, response):
-        title = response.css("title::text").extract()
-        yield {"titletext": title}
+        imoveis = response.css("#property-listing .col-lg-3")
+
+        for imovel in imoveis:
+            yield{
+                'name' : imovel.css("div h3 a::text").get(),
+                'price' : imovel.css("div.price span::text").get(),
+                'bairro' : imovel.css("h3 small::text").get(),
+            }
+        
+        next_page = response.xpath('//li[9]/a/@href').get()  
+        
+        if next_page is not None:
+            next_page_url = "https://www.imobiliariaativa.com.br" + next_page
+            yield scrapy.Request(response.urljoin(next_page_url))
+            #yield response.follow(next_page_url, callback= self.parse), another method for turn pages.
+
+
+    
+        
+        
