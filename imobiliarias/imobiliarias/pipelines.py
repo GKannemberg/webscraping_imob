2d4b4fe941716b8cc54e3a906fda5d8611cacd4c
Eduardo Lovera Hirano <eduardo.hirano@eemovel.com>
2024-03-11 19:36:56 -0300

    ref: pipelines normalization

--- imobiliarias/imobiliarias/pipelines.py
+++ imobiliarias/imobiliarias/pipelines.py
@@ -7,7 +7,40 @@
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
 
+title_translation = {
+    "Apartamento Padrão": {"category": "apartment", "sub_category": "standard"},
+    "Casa Padrão": {"category": "apartment", "sub_category": "standard"},
+    "Comercial Edificio Co...": {"category": "apartment", "sub_category": "standard"},
+    "Terreno Lote Urbano": {"category": "apartment", "sub_category": "standard"},
+    "Rural Sítio": {"category": "apartment", "sub_category": "standard"},
+}
+
 
 class ImobiliariasPipeline:
     def process_item(self, item, spider):
+        adapter = ItemAdapter(item)
+
+        standartize_title(adapter)
+
+        normalize_price(adapter)
+
+        items["title"] = title
+        items["price"] = price
+        items["bairro"] = bairro
+
         return item
+
+
+def standartize_title(adapter):
+    title_string = adapter.get("title")
+    dict_translation = title_translation[title_string]
+
+    adapter["category"] = dict_translation["category"]
+    adapter["sub_category"] = dict_translation["sub_category"]
+
+
+def normalize_price(adapter):
+    price_string = adapter.get("price")
+
+    price_int = int(price_string.split(",")[0].replace(".", ""))
+    adapter["price"] = price_int
