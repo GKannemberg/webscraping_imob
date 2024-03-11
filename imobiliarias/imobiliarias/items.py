f56890f453aac94b65520dec8e1da527d896ddac
Gustavo Kanenemberg <gkannemberg@gmail.com>
2024-03-11 18:42:16 -0300

    feat: add scrapy item

--- imobiliarias/imobiliarias/items.py
+++ imobiliarias/imobiliarias/items.py
@@ -8,5 +8,7 @@ import scrapy
 
 class ImobiliariasItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    title = scrapy.Field()
+    price = scrapy.Field()
+    bairro = scrapy.Field()
+    
--- imobiliarias/imobiliarias/spiders/quotes.py
+++ imobiliarias/imobiliarias/spiders/quotes.py
@@ -1,5 +1,5 @@
 import scrapy
-
+from imobiliarias.items import ImobiliariasItem
 
 class QuoteSpider(scrapy.Spider):
     name = "quotes"
@@ -8,19 +8,29 @@ class QuoteSpider(scrapy.Spider):
 
     def parse(self, response):
         imoveis = response.css("#property-listing .col-lg-3")
-
+        items = ImobiliariasItem()
+        
         for imovel in imoveis:
-            yield{
-                'name' : imovel.css("div h3 a::text").get(),
-                'price' : imovel.css("div.price span::text").get(),
-                'bairro' : imovel.css("h3 small::text").get(),
-            }
+            title = imovel.css("div h3 a::text").get(),
+            price = imovel.css("div.price span::text").get(),
+            bairro = imovel.css("h3 small::text").get(),
+            
+            items['title'] = title
+            items['price'] = price
+            items['bairro'] = bairro
+            
+            yield items
+                
+            
         
         next_page = response.xpath('//li[9]/a/@href').get()  
         
         if next_page is not None:
             next_page_url = "https://www.imobiliariaativa.com.br" + next_page
             yield scrapy.Request(response.urljoin(next_page_url))
+            
+            
+            
             #yield response.follow(next_page_url, callback= self.parse), another method for turn pages.
 
 
